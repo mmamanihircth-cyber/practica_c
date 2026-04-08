bbeec425cbb4965a9e2dc122d321400ea50dfbd9
matias.hirch <mmamanihircth@frba.utn.edu.ar>
2026-04-07 22:54:35 -0300

    Primer TP C++

---
+++ prueba.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nombre;
+    cout << "¿Cual es tu nombre?"<<endl;
+    cin >> nombre;
+    cout << "Un gusto conocerte " << nombre <<endl;
+    return 0;
+}
